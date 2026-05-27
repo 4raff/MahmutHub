9e086446b37a51504620288bf5cb07c0cab3cea5
Ahmad Raffi Arasy <araffiarsy@student.telkomuniversity.ac.id>
2026-05-28 02:22:04 +0700

    added more delay in auto answer

--- main.lua
+++ main.lua
@@ -4,11 +4,11 @@ print("Checking game support...")
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
 
 local supportedGames = {
-    [9186719164] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/games/SailorPiece/production/main.lua",
-    [1281592938] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/games/EntrenchedWW1/production/main.lua",
-    [7633926880] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/games/BloxStrike/production/main.lua",
-    [10004244222] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/games/KickALuckyBlock/production/main.lua",
-    [9967681734] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/games/TebakLagu/production/main.lua",
+    [9186719164] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/SailorPiece/production/main.lua",
+    [1281592938] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/EntrenchedWW1/production/main.lua",
+    [7633926880] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/BloxStrike/production/main.lua",
+    [10004244222] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/KickALuckyBlock/production/main.lua",
+    [9967681734] = "https://raw.githubusercontent.com/4raff/MahmutHub/refs/heads/main/TebakLagu/production/main.lua",
 }
 
 if supportedGames[UniverseID] then 
